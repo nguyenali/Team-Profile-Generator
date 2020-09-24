0db4a30602560794ab44a24805767849fe8f26af
Ali Nguyen <alin57717@gmail.com>
2020-09-24 15:56:02 -0700

    create module for Employee infoermation

---
+++ test/Employee.test.js
@@ -0,0 +1,18 @@
+class Employeee {
+    constructor(name,id,email) {
+        this.name = name;
+        this.id = id;
+        this email = email;
+    }
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.id;
+    }
+    getEmail(){
+        return this.email;
+    }
+}
+
+module.exports = Employeee
