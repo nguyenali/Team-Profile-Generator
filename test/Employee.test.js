0193fb7531d7d8d57fcc031f7a0f18b321a3b08a
Ali Nguyen <alin57717@gmail.com>
2020-09-24 16:02:58 -0700

    create and export engineer class and to inherit from Employee

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,18 +1,1 @@
-class Employeee {
-    constructor(name,id,email) {
-        this.name = name;
-        this.id = id;
-        this email = email;
-    }
-    getName(){
-        return this.name;
-    }
-    getId(){
-        return this.id;
-    }
-    getEmail(){
-        return this.email;
-    }
-}
-
-module.exports = Employeee
+module.exports = Employee
