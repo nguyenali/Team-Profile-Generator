572bbb235d2a6a2bf23863f70825fe014574b467
Ali Nguyen <alin57717@gmail.com>
2020-09-24 16:56:32 -0700

    create function to get manager infomation through question prompts

--- index.js
+++ index.js
@@ -2,3 +2,43 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Employee");
 const Intern = require("./lib/intern");
 const inquirer =require("inquirer");
+
+
+
+const teamMember = [];
+
+function index() {
+    function getManager(){
+    inquirer.prompt({
+    {
+        type:"input",
+        name: "managerName";
+        message: "What is your Manager's name?"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is your Id?"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your Email?"
+    },
+    {
+        type:'input',
+        name:"managerofficeNumber",
+        message: "What is the office number for the manageer?"
+    }
+    }).then(response => {
+        const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerofficeNumber);
+        teamMember.push(manager);
+        addingnewMember();
+    })
+
+}
+    
+
+
+
+}
