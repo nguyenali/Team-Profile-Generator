be3c07d0d6ecc5c6213b5dd3201d5d1341f2251e
Ali Nguyen <alin57717@gmail.com>
2020-09-25 23:59:00 -0700

    create test to verify manager function

---
+++ test/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+test("Can get office number via constructor",() => {
+    const testValue = "officeNumber";
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+} )
+
+
+test("getPosition() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Foo", 1, "test@test.com", 500);
+    expect(e.getPosition()).toBe(testValue);
+}
+
+
+test("Can get office number  via getofficeNumber()", () => {
+    const testValue = "officeNumber";
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.getofficeNumber()).toBe(testValue);
+});
